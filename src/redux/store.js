8424e641f5d74f680ff86c173b2bae03c18e513b
Benjamin garrido <benjamin.garrido@solera.com>
2023-01-31 16:49:43 +0100

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,22 @@
+import {createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import userReducer from './userDuck';
+import thunk from 'redux-thunk';
+import charsReducer, {getCharactersAction} from './charsDuck'
+
+let rootReducer= combineReducers({
+    user: userReducer,
+    characters:charsReducer,
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function generateStore() {
+    let store = createStore(
+        rootReducer,
+        composeEnhancers(applyMiddleware(thunk))
+    )
+    //get chars first time
+    getCharactersAction()(store.dispatch,store.getState)
+    return store
+
+}
